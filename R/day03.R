7cb1dcb55d8ee8c0b6c413fffe7d417294236548
nmoorenz <a.message.to.nate@gmail.com>
2021-12-05 21:44:04 +1300

    clean up early days, complete day 3, halfway through day 4, download day 5

--- R/day03.R
+++ R/day03.R
@@ -64,31 +64,199 @@
 #' decimal, not binary.)
 #'
 #' **Part Two**
+#' Next, you should verify the *life support rating*, which can be
+#' determined by multiplying the *oxygen generator rating* by the *CO2
+#' scrubber rating*.
 #'
-#' *(Use have to manually add this yourself.)*
+#' Both the oxygen generator rating and the CO2 scrubber rating are values
+#' that can be found in your diagnostic report - finding them is the tricky
+#' part. Both values are located using a similar process that involves
+#' filtering out values until only one remains. Before searching for either
+#' rating value, start with the full list of binary numbers from your
+#' diagnostic report and *consider just the first bit* of those numbers.
+#' Then:
 #'
-#' *(Try using `convert_clipboard_html_to_roxygen_md()`)*
+#' -   Keep only numbers selected by the *bit criteria* for the type of
+#'     rating value for which you are searching. Discard numbers which do
+#'     not match the bit criteria.
+#' -   If you only have one number left, stop; this is the rating value for
+#'     which you are searching.
+#' -   Otherwise, repeat the process, considering the next bit to the
+#'     right.
 #'
-#' @param x some data
-#' @return For Part One, `f03a(x)` returns .... For Part Two,
-#'   `f03b(x)` returns ....
+#' The *bit criteria* depends on which type of rating value you want to
+#' find:
+#'
+#' -   To find *oxygen generator rating*, determine the *most common* value
+#'     (`0` or `1`) in the current bit position, and keep only numbers with
+#'     that bit in that position. If `0` and `1` are equally common, keep
+#'     values with a `1` in the position being considered.
+#' -   To find *CO2 scrubber rating*, determine the *least common* value
+#'     (`0` or `1`) in the current bit position, and keep only numbers with
+#'     that bit in that position. If `0` and `1` are equally common, keep
+#'     values with a `0` in the position being considered.
+#'
+#' For example, to determine the *oxygen generator rating* value using the
+#' same example diagnostic report from above:
+#'
+#' -   Start with all 12 numbers and consider only the first bit of each
+#'     number. There are more `1` bits (7) than `0` bits (5), so keep only
+#'     the 7 numbers with a `1` in the first position: `11110`, `10110`,
+#'     `10111`, `10101`, `11100`, `10000`, and `11001`.
+#' -   Then, consider the second bit of the 7 remaining numbers: there are
+#'     more `0` bits (4) than `1` bits (3), so keep only the 4 numbers with
+#'     a `0` in the second position: `10110`, `10111`, `10101`, and
+#'     `10000`.
+#' -   In the third position, three of the four numbers have a `1`, so keep
+#'     those three: `10110`, `10111`, and `10101`.
+#' -   In the fourth position, two of the three numbers have a `1`, so keep
+#'     those two: `10110` and `10111`.
+#' -   In the fifth position, there are an equal number of `0` bits and `1`
+#'     bits (one each). So, to find the *oxygen generator rating*, keep the
+#'     number with a `1` in that position: `10111`.
+#' -   As there is only one number left, stop; the *oxygen generator
+#'     rating* is `10111`, or `23` in decimal.
+#'
+#' Then, to determine the *CO2 scrubber rating* value from the same example
+#' above:
+#'
+#' -   Start again with all 12 numbers and consider only the first bit of
+#'     each number. There are fewer `0` bits (5) than `1` bits (7), so keep
+#'     only the 5 numbers with a `0` in the first position: `00100`,
+#'     `01111`, `00111`, `00010`, and `01010`.
+#' -   Then, consider the second bit of the 5 remaining numbers: there are
+#'     fewer `1` bits (2) than `0` bits (3), so keep only the 2 numbers
+#'     with a `1` in the second position: `01111` and `01010`.
+#' -   In the third position, there are an equal number of `0` bits and `1`
+#'     bits (one each). So, to find the *CO2 scrubber rating*, keep the
+#'     number with a `0` in that position: `01010`.
+#' -   As there is only one number left, stop; the *CO2 scrubber rating* is
+#'     `01010`, or `10` in decimal.
+#'
+#' Finally, to find the life support rating, multiply the oxygen generator
+#' rating (`23`) by the CO2 scrubber rating (`10`) to get `230`.
+#'
+#' Use the binary numbers in your diagnostic report to calculate the oxygen
+#' generator rating and CO2 scrubber rating, then multiply them together.
+#' *What is the life support rating of the submarine?* (Be sure to
+#' represent your answer in decimal, not binary.)
+#'
+#' @param x a list of binary instructions for the submarine
+#' @return For Part One, `f03a(x)` returns the power consumption, gamma rate multiplied by
+#' the epsilon rate
+#' For Part Two, `f03b(x)` returns oxygen rating multiplied by carbon dioxide rating
 #' @export
 #' @examples
 #' f03a(example_data_03())
 #' f03b()
 f03a <- function(x) {
-  bins = str_split(x, "", 12)
+  # split into individual columns
+  bins = str_split_fixed(x, "", 12) %>% as.data.frame()
+
+  # need them to be numeric
+  bin_num = sapply(bins, as.numeric)
+
+  gamma_rate =  strtoi(paste(as.numeric(colSums(bin_num) >= 500), collapse = ""), base = 2)
+
+  epsilon_rate = strtoi(paste(as.numeric(colSums(bin_num) < 500), collapse = ""), base = 2)
+
+  part_one = gamma_rate * epsilon_rate
 }
 
 
 #' @rdname day03
 #' @export
 f03b <- function(x) {
+  # width of one string
+  wdth = nchar(x[[1]])
 
+  # split into individual columns
+  bins = str_split_fixed(x, "", wdth) %>% as.data.frame()
+
+  # need them to be numeric
+  bin_num = sapply(bins, as.numeric)
+
+  # recursive function call
+  oxygen_binary = return_oxygen(bin_num)
+
+  # convert to integer for oxygen
+  oxygen_rating = strtoi(paste(oxygen_binary, collapse = ""), base = 2)
+
+  # recursive with less than, c02
+  c02_binary = return_c02(bin_num)
+
+  # convert to integer
+  c02_rating = strtoi(paste(c02_binary, collapse = ""), base = 2)
+
+  # answer
+  oxygen_rating * c02_rating
 }
 
 
-f03_helper <- function(x) {
+return_oxygen <- function(z, colnum = 1) {
+  # z is a matrix of binary digits
+  # calculate the length
+  # then which digit is more common in the column
+  # input of colnum
+
+  # number of rows of array
+  lngth = nrow(z)
+
+  # wdth for stopping
+  wdth = ncol(z)
+
+  # column with greater number of digits
+  dgt = as.integer(sum(z[,colnum]) >= ceiling(lngth / 2))
+
+  # filter (is that the right word for non-tidyverse?)
+  row_bool = z[,colnum] == dgt
+  z = z[row_bool, , drop = FALSE]
+
+  # return if we are at the end
+  if (colnum == wdth) {
+    print("colnum = 12")
+    return(z)
+  }
+
+  if (nrow(z) == 1) {
+    print("only one row")
+    return(z)
+  }
+
+  # increment by 1
+  colnum = colnum + 1
+
+  # call same function again with new column to check
+  z = return_oxygen(z, colnum)
+
+}
+
+return_c02 <- function(z, colnum = 1) {
+  # this is the same as above but with less than sign
+
+  # number of rows of array
+  lngth = nrow(z)
+
+  # wdth for stopping
+  wdth = ncol(z)
+
+  # column with greater number of digits
+  dgt = as.integer(sum(z[,colnum]) < ceiling(lngth / 2))
+
+  # filter (is that the right word for non-tidyverse?)
+  row_bool = z[,colnum] == dgt
+  z = z[row_bool, , drop = FALSE]
+
+  # return if we are at the end
+  if (colnum == wdth) return(z)
+
+  if (nrow(z) == 1) return(z)
+
+  # increment by 1
+  colnum = colnum + 1
+
+  # call same function again with new column to check
+  z = return_c02(z, colnum)
 
 }
 
@@ -99,10 +267,18 @@ f03_helper <- function(x) {
 #' @export
 example_data_03 <- function(example = 1) {
   l <- list(
-    a = c(
-
-
-    )
+    a = c("00100",
+       "11110",
+       "10110",
+       "10111",
+       "10101",
+       "01111",
+       "00111",
+       "11100",
+       "10000",
+       "11001",
+       "00010",
+       "01010")
   )
   l[[example]]
 }
--- inst/run-day03.R
+++ inst/run-day03.R
@@ -1,4 +1,6 @@
 library(adventofcode21)
+library(tidyverse)
+
 x <- readLines("./inst/input03.txt")
 
 p1 <- f03a(x)
@@ -6,3 +8,9 @@ p2 <- f03b(x)
 
 stopifnot(p1 == aoc_solutions$day03a)
 stopifnot(p2 == aoc_solutions$day03b)
+
+# part two
+# attempt 1
+# 4205097
+# attempt 2
+# 1877139
